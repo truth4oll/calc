ee7a888900cd72cd384dcb840eee149a012beaf3
truth4oll <truth4oll@gmail.com>
2011-02-02 11:44:48 +0300

    сплит с android_first

--- gen/com/example/R.java
+++ gen/com/example/R.java
@@ -13,10 +13,31 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int clear=0x7f06000f;
+        public static final int division=0x7f060004;
+        public static final int minus=0x7f060002;
+        public static final int multiply=0x7f060003;
+        public static final int num0=0x7f06000e;
+        public static final int num1=0x7f060005;
+        public static final int num2=0x7f060006;
+        public static final int num3=0x7f060007;
+        public static final int num4=0x7f060008;
+        public static final int num5=0x7f060009;
+        public static final int num6=0x7f06000a;
+        public static final int num7=0x7f06000b;
+        public static final int num8=0x7f06000c;
+        public static final int num9=0x7f06000d;
+        public static final int sum=0x7f060001;
+        public static final int text=0x7f060000;
+    }
+    public static final class integer {
+        public static final int widget11=0x7f040000;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
+        public static final int app_name=0x7f050000;
     }
 }
--- src/com/example/FirstActivity.java
+++ src/com/example/FirstActivity.java
@@ -2,14 +2,121 @@ package com.example;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class FirstActivity extends Activity
 {
-    /** Called when the activity is first created. */
+    private int arg1, action; // action: sum=1, minus=2, multiply=3, division=4
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+
+
+    public void numButton(View view) {
+        String string;
+        TextView txt = (TextView) findViewById(R.id.text);
+
+        switch (view.getId()) {
+            case R.id.num1:
+                string = txt.getText() + Integer.toString(1);
+                txt.setText(string);
+                break;
+            case R.id.num2:
+                string = txt.getText() + Integer.toString(2);
+                txt.setText(string);
+                break;
+            case R.id.num3:
+                string = txt.getText() + Integer.toString(3);
+                txt.setText(string);
+                break;
+            case R.id.num4:
+                string = txt.getText() + Integer.toString(4);
+                txt.setText(string);
+                break;
+            case R.id.num5:
+                string = txt.getText() + Integer.toString(5);
+                txt.setText(string);
+                break;
+            case R.id.num6:
+                string = txt.getText() + Integer.toString(6);
+                txt.setText(string);
+                break;
+            case R.id.num7:
+                string = txt.getText() + Integer.toString(7);
+                txt.setText(string);
+                break;
+            case R.id.num8:
+                string = txt.getText() + Integer.toString(8);
+                txt.setText(string);
+                break;
+            case R.id.num9:
+                string = txt.getText() + Integer.toString(9);
+                txt.setText(string);
+                break;
+            case R.id.num0:
+                string = txt.getText() + Integer.toString(0);
+                txt.setText(string);
+                break;
+        }
+    }
+
+    public void actionButton(View view){
+        TextView txt = (TextView) findViewById(R.id.text);
+        arg1 = Integer.parseInt((String) txt.getText());
+        switch (view.getId()){
+            case R.id.sum:
+                action = 1;
+                txt.setText("");
+                break;
+            case R.id.minus:
+                action = 2;
+                txt.setText("");
+                break;
+            case R.id.multiply:
+                action = 3;
+                txt.setText("");
+                break;
+            case R.id.division:
+                action = 4;
+                txt.setText("");
+                break;
+        }
+    }
+
+    public void summaryButton(View view){
+        TextView txt = (TextView) findViewById(R.id.text);
+
+        if (action == 1) {
+         //   sum = arg1+Integer.parseInt((String) txt.getText());
+            txt.setText(Integer.toString(arg1+Integer.parseInt((String) txt.getText())));
+            action = 0;
+        }
+        if (action == 2) {
+       //     sum = arg1-Integer.parseInt((String) txt.getText());
+            txt.setText(Integer.toString(arg1-Integer.parseInt((String) txt.getText())));
+            action = 0;
+        }
+        if (action == 3) {
+     //       sum = arg1*Integer.parseInt((String) txt.getText());
+            txt.setText(Integer.toString(arg1*Integer.parseInt((String) txt.getText())));
+            action = 0;
+        }
+        if (action == 4) {
+   //         sum = arg1/Integer.parseInt((String) txt.getText());
+            txt.setText(Integer.toString(arg1/Integer.parseInt((String) txt.getText())));
+            action = 0;
+        }
+    }
+
+    public void clearButton(View view){
+        TextView txt = (TextView) findViewById(R.id.text);
+        arg1 = action = 0;
+        txt.setText("");
+    }
+
 }
