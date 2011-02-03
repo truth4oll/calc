2e93e7c9f08675b35aa475e0909e3d1edc1b3b24
truth4oll <truth4oll@gmail.com>
2011-02-03 16:09:27 +0300

    Добавлена строка истории вычислений, пофикшена бага №1 в тимере

--- src/com/example/FirstActivity.java
+++ src/com/example/FirstActivity.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 public class FirstActivity extends Activity
 {
     private int arg1, action; // action: sum=1, minus=2, multiply=3, division=4
+    private boolean isCounted=false;
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
@@ -18,105 +19,118 @@ public class FirstActivity extends Activity
 
 
     public void numButton(View view) {
-        String string;
+        String string="";
         TextView txt = (TextView) findViewById(R.id.text);
-
+        TextView txtHistory = (TextView)findViewById(R.id.history);
+         if (this.isCounted){
+             this.clearAction();
+             this.isCounted=false;
+         }
         switch (view.getId()) {
             case R.id.num1:
                 string = txt.getText() + Integer.toString(1);
-                txt.setText(string);
                 break;
             case R.id.num2:
                 string = txt.getText() + Integer.toString(2);
-                txt.setText(string);
                 break;
             case R.id.num3:
                 string = txt.getText() + Integer.toString(3);
-                txt.setText(string);
                 break;
             case R.id.num4:
                 string = txt.getText() + Integer.toString(4);
-                txt.setText(string);
                 break;
             case R.id.num5:
                 string = txt.getText() + Integer.toString(5);
-                txt.setText(string);
                 break;
             case R.id.num6:
                 string = txt.getText() + Integer.toString(6);
-                txt.setText(string);
                 break;
             case R.id.num7:
                 string = txt.getText() + Integer.toString(7);
-                txt.setText(string);
                 break;
             case R.id.num8:
                 string = txt.getText() + Integer.toString(8);
-                txt.setText(string);
                 break;
             case R.id.num9:
                 string = txt.getText() + Integer.toString(9);
-                txt.setText(string);
                 break;
             case R.id.num0:
                 string = txt.getText() + Integer.toString(0);
-                txt.setText(string);
                 break;
         }
+        txt.setText(string);
+        txtHistory.setText(txtHistory.getText() + string);
+
     }
 
     public void actionButton(View view){
+        String string="";
+        TextView txtHistory = (TextView)findViewById(R.id.history);
         TextView txt = (TextView) findViewById(R.id.text);
         arg1 = Integer.parseInt((String) txt.getText());
         switch (view.getId()){
             case R.id.sum:
                 action = 1;
-                txt.setText("");
+                string = "+";
                 break;
             case R.id.minus:
                 action = 2;
-                txt.setText("");
+                string = "-";
                 break;
             case R.id.multiply:
                 action = 3;
-                txt.setText("");
+                string = "*";
                 break;
             case R.id.division:
                 action = 4;
-                txt.setText("");
+                string = "/";
                 break;
         }
+        txt.setText("");
+        txtHistory.setText(txtHistory.getText() + string);
     }
 
     public void summaryButton(View view){
+        String sResult = "";
         TextView txt = (TextView) findViewById(R.id.text);
+        TextView txtHistory = (TextView)findViewById(R.id.history);
+
 
         if (action == 1) {
-         //   sum = arg1+Integer.parseInt((String) txt.getText());
-            txt.setText(Integer.toString(arg1+Integer.parseInt((String) txt.getText())));
+            sResult =    Integer.toString(arg1+Integer.parseInt((String) txt.getText()));
+            txt.setText(sResult);
             action = 0;
         }
         if (action == 2) {
-       //     sum = arg1-Integer.parseInt((String) txt.getText());
-            txt.setText(Integer.toString(arg1-Integer.parseInt((String) txt.getText())));
+            sResult =  Integer.toString(arg1-Integer.parseInt((String) txt.getText()));
+            txt.setText(sResult);
             action = 0;
         }
         if (action == 3) {
-     //       sum = arg1*Integer.parseInt((String) txt.getText());
-            txt.setText(Integer.toString(arg1*Integer.parseInt((String) txt.getText())));
+            sResult =    Integer.toString(arg1*Integer.parseInt((String) txt.getText()));
+            txt.setText(sResult);
             action = 0;
         }
         if (action == 4) {
-   //         sum = arg1/Integer.parseInt((String) txt.getText());
-            txt.setText(Integer.toString(arg1/Integer.parseInt((String) txt.getText())));
+            sResult =    Integer.toString(arg1/Integer.parseInt((String) txt.getText()));
+            txt.setText(sResult);
             action = 0;
         }
+        this.isCounted = true;
+        txtHistory.setText(txtHistory.getText() + "=" + sResult );
     }
 
     public void clearButton(View view){
+          this.clearAction();
+    }
+
+
+    private void clearAction(){
         TextView txt = (TextView) findViewById(R.id.text);
+        TextView txtHistory = (TextView)findViewById(R.id.history);
         arg1 = action = 0;
         txt.setText("");
+        txtHistory.setText("");
     }
 
 }
